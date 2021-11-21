35d49bcbc8ea7d030a884c1982cbe5a3185b48b8
victornpb <3372598+victornpb@users.noreply.github.com>
2021-11-21 10:32:51 -0300

    public api

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-import { Shortcuts } from './shortcuts';
-export default Shortcuts;
+import { Shortcuts, ComponentFocusManager } from './shortcuts';
+import { Hotkeys, announceHotkeys } from './hotkeys';
+
+
+export default {
+    Shortcuts,
+    ComponentFocusManager,
+    Hotkeys,
+    announceHotkeys,
+};
