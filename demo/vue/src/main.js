ea5b71060d9edb585d4c632eb31e6e111d6b087c
victornpb <3372598+victornpb@users.noreply.github.com>
2022-07-09 19:14:44 -0300

    inbox demo

--- demo/vue/src/main.js
+++ demo/vue/src/main.js
@@ -1,7 +1,7 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import '@mdi/font/css/materialdesignicons.css'
+import Vue from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
+import '@mdi/font/css/materialdesignicons.css';
 
 import kbdl from '../../../dist/keyboardly.esm.mjs'; // Keyboardly
 import '../../../dist/style.css';
@@ -10,12 +10,15 @@ import router from './router';
 
 window.hotkeys = new kbdl.Hotkeys();
 
-Vue.config.productionTip = false
+window.shortcuts = new kbdl.Shortcuts();
+
+
+Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
 
 
--- demo/vue/src/pages/inbox/index.vue
+++ demo/vue/src/pages/inbox/index.vue
@@ -2,70 +2,78 @@
   <v-app id="inspire">
     <v-system-bar app>
       <v-spacer></v-spacer>
-
       <v-icon>mdi-square</v-icon>
-
       <v-icon>mdi-circle</v-icon>
-
       <v-icon>mdi-triangle</v-icon>
     </v-system-bar>
 
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-    >
-      <v-sheet
-        color="grey lighten-4"
-        class="pa-4"
-      >
-        <v-avatar
-          class="mb-4"
-          color="grey darken-1"
-          size="64"
-        ></v-avatar>
-
-        <div>john@vuetifyjs.com</div>
+    <v-navigation-drawer v-model="drawer" app permanent>
+      <v-sheet color="grey lighten-4" class="pa-4">
+        <v-avatar class="mb-4" color="grey darken-1" size="64"></v-avatar>
+        <div>user@example.com</div>
       </v-sheet>
-
-      <v-divider></v-divider>
+      <v-divider />
 
       <v-list>
-        <v-list-item
-          v-for="[icon, text] in links"
-          :key="icon"
-          link
-        >
-          <v-list-item-icon>
-            <v-icon>{{ icon }}</v-icon>
-          </v-list-item-icon>
 
+        <v-list-item link data-hotkey="i">
+          <v-list-item-icon>
+            <v-icon>mdi-inbox-arrow-down</v-icon>
+          </v-list-item-icon>
           <v-list-item-content>
-            <v-list-item-title>{{ text }}</v-list-item-title>
+            <v-list-item-title>Inbox</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
+
+        <v-list-item link data-hotkey="s">
+          <v-list-item-icon>
+            <v-icon>mdi-star</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Starred</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link data-hotkey="o">
+          <v-list-item-icon>
+            <v-icon>mdi-send</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Sent Mail</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link data-hotkey="d">
+          <v-list-item-icon>
+            <v-icon>mdi-file-document-box</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Drafts</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link data-hotkey="t">
+          <v-list-item-icon>
+            <v-icon>mdi-delete</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Trash</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
       </v-list>
     </v-navigation-drawer>
 
     <v-main>
-      <v-container
-        class="py-8 px-6"
-        fluid
-      >
+      <v-container class="py-8 px-6" fluid>
         <v-row>
-          <v-col
-            v-for="card in cards"
-            :key="card"
-            cols="12"
-          >
+          <v-col v-for="card in cards" :key="card" cols="12">
             <v-card>
               <v-subheader>{{ card }}</v-subheader>
 
               <v-list two-line>
                 <template v-for="n in 6">
-                  <v-list-item
-                    :key="n"
-                    data-shortcut-component
-                  >
+                  <v-list-item :key="n" data-shortcut-component>
                     <v-list-item-avatar color="grey darken-1">
                     </v-list-item-avatar>
 
@@ -73,7 +81,8 @@
                       <v-list-item-title>Message {{ n }}</v-list-item-title>
 
                       <v-list-item-subtitle>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil repellendus distinctio similique
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Nihil repellendus distinctio similique
                       </v-list-item-subtitle>
                       <v-list-item-subtitle>
                         <v-btn icon>
@@ -83,11 +92,7 @@
                     </v-list-item-content>
                   </v-list-item>
 
-                  <v-divider
-                    v-if="n !== 6"
-                    :key="`divider-${n}`"
-                    inset
-                  ></v-divider>
+                  <v-divider v-if="n !== 6" :key="`divider-${n}`" inset />
                 </template>
               </v-list>
             </v-card>
@@ -99,16 +104,10 @@
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      cards: ['Today', 'Yesterday'],
-      drawer: null,
-      links: [
-        ['mdi-inbox-arrow-down', 'Inbox'],
-        ['mdi-send', 'Send'],
-        ['mdi-delete', 'Trash'],
-        ['mdi-alert-octagon', 'Spam'],
-      ],
-    }),
-  }
+export default {
+  data: () => ({
+    cards: ["Today", "Yesterday"],
+    drawer: true,
+  }),
+};
 </script>
